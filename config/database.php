b867eafe0a7dd3f2d9a2400d70ae6aa87e31fea9
redheadchloe <chloe.socrates.7z@gmail.com>
2022-09-11 22:07:29 +0900

    update

--- config/database.php
+++ config/database.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_HOST', 'localhost');
-define('DB_USER', 'chloe');
-define('DB_PASS', '123456');
-define('DB_NAME', 'php');
+define('DB_USER', 'bychngiz_php-feedback-user');
+define('DB_PASS', 'vurtH7X7jawSDVp');
+define('DB_NAME', 'bychngiz_php-feedback');
 
 //create connection
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
